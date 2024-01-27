21dc910e457afb30bf362dd4530b4ed932b7cbf1
Yousse31 <80832870+Yousse31@users.noreply.github.com>
2024-01-27 21:19:43 +0100

    main task accomplished grace à nono et youyou

--- noem-app/src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ noem-app/src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -21,14 +21,18 @@ export class AssignmentDetailComponent implements OnInit {
   mapImageProf = new Map<string, string>();
 
   listeNomProf = ["Arnault", "Galli", "Tounsi", "Buffa", "Donati"];
-  listeImageMatière = ["Communication.jpg", "Base_de_donnees.jpg", "Management_du_numerique.jpg", "IA.jpg","Outils_mathematiques.jpg"]
+  listeImageMatiere = ["Communication.jpg", "Base_de_donnees.jpg", "Management_du_numerique.jpg", "IA.jpg","Outils_mathematiques.jpg"]
   listeImageProf = ["Frederic_Arnault.jpeg","Gregory_Galli.jpeg","Stephane_Tounsi.jpeg","Michel_Buffa.jpeg","Leo_Donati.jpeg"]
-  listeMatiere = ["Communication","Base de données","Management du numérique", "IA","Outils mathématiques"]
+  listeMatiere = ["Communication","Base de donnees","Management du numerique", "IA","Outils mathematiques"]
+
+  
 
   constructor(public authService : AuthService, private assignmentsService: AssignmentsService, private route: ActivatedRoute, private router:Router) {}
 
   ngOnInit(): void {
     const id= +this.route.snapshot.params['id'];
+
+    this.remplirMaps();
 
     this.assignmentsService.getAssignment(id)
       .subscribe(a =>this.assignmentTransmis = a);
@@ -79,33 +83,58 @@ export class AssignmentDetailComponent implements OnInit {
   }
 
   selectImageProf(): string {
-    const path = "../../assets/imageProf/"
-    switch(this.assignmentTransmis.matiere) { 
-      case "Communication": { 
-        return path+"Frederic_Arnault.jpeg";
-         break; 
-      } 
-      case "Base de données": { 
-        return path+"Gregory_Galli.jpeg";
-         break; 
-      } 
-      case "Donati": { 
-        return path+"Leo_Donati.jpeg";
-         break; 
-      } 
-      case "Buffa": { 
-        return path+"Leo_Donati.jpeg";
-         break; 
-      } 
-      case "Tounsi": { 
-        return path+"Leo_Donati.jpeg";
-         break; 
-      } 
-      default: { 
-        return null;
-        break; 
-      } 
-   } 
+    const path = "../../assets/imageProf/";
+    const matiere = this.assignmentTransmis.matiere;
+  
+    console.log("Matiere:", matiere);
+  
+    if (this.mapImageProf.has(matiere)) {
+      const imagePath = path + this.mapImageProf.get(matiere);
+      console.log("Image Path:", imagePath);
+      return imagePath;
+    } else {
+      console.log("Matiere non trouvée dans la mapImageProf.");
+      return null;
+    }
+  }
+
+  selectNomProf(): string {
+    if (this.assignmentTransmis) {
+      const matiere = this.assignmentTransmis.matiere;
+      if (this.mapNomProf.has(matiere)) {
+        return this.mapNomProf.get(matiere);
+      }
+    }
+    return null;
+  }
+
+  selectImageMatiere(): string {
+    const path = "../../assets/imageMatiere/";
+    const matiere = this.assignmentTransmis.matiere;
+  
+    console.log("Matiere:", matiere);
+  
+    if (this.mapImageProf.has(matiere)) {
+      const imagePath = path + this.mapImageMatiere.get(matiere);
+      console.log("Image Path:", imagePath);
+      return imagePath;
+    } else {
+      console.log("Matiere non trouvée dans la mapImageProf.");
+      return null;
+    }
+  }
+
+  private remplirMaps(): void {
+    for (let i = 0; i < this.listeNomProf.length; i++) {
+      const nomProf = this.listeNomProf[i];
+      const imageMatiere = this.listeImageMatiere[i];
+      const imageProf = this.listeImageProf[i];
+      const matiere = this.listeMatiere[i];
+
+      this.mapNomProf.set(matiere, nomProf);
+      this.mapImageMatiere.set(matiere, imageMatiere);
+      this.mapImageProf.set(matiere, imageProf);
+    }
   }
 
 }
