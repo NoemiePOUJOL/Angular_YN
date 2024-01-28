b0f74ab46bd942c9e98f6d1d70027f050e83af7d
Yousse31 <80832870+Yousse31@users.noreply.github.com>
2024-01-28 12:37:44 +0100

    url2

--- noem-app/src/app/shared/assignments.service.ts
+++ noem-app/src/app/shared/assignments.service.ts
@@ -15,8 +15,8 @@ export class AssignmentsService {
 
   assignments : Assignment[] = []
 
-  url = "http://localhost:8010/api/assignments";
-  //url = "https://serverjs-naxx.onrender.com/api/assignments";
+  //url = "http://localhost:8010/api/assignments";
+  url = "https://serverjs-naxx.onrender.com/api/assignments";
 
   getAssignments(): Observable<Assignment[]>{
     return this.http.get<Assignment[]>(this.url); 
